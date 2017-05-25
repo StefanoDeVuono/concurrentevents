5d078eb593e3b0f49d9cd8101a39bf863b6fda87
Stefano De Vuono <stefano@applepiecapital.com>
2017-05-25 11:11:23 -0700

    test simultaneous cases

--- test.js
+++ test.js
@@ -36,8 +36,7 @@ test('once-twice pattern: ready fires once then interval starts firing', assert 
   });
 
   assert.readyThenIntervals(emitterReady, emitterInterval, acknowledgement)
-
-})
+});
 
 
 test('once-twice pattern: interval starts firing then ready fires once', assert => {
@@ -63,7 +62,32 @@ test('once-twice pattern: interval starts firing then ready fires once', assert 
   });
 
   assert.intervalsThenReady(emitterReady, emitterInterval, acknowledgement);
+});
 
+
+test('once-twice pattern: both interval and ready start firing at the same time', assert => {
+  const emitterReady = new EventEmitter(),
+        emitterInterval = new EventEmitter(),
+        acknowledgement = td.function('.acknowledgement');
+
+  let ready = false;
+
+  emitterReady.once('ready', ()=>{
+    ready = true;
+  });
+
+  emitterInterval.on('interval', ()=>{
+    if (ready){
+      acknowledgement()
+    } else {
+      emitterReady.once('ready', ()=>{
+        ready = true;
+        acknowledgement()
+      });
+    }
+  });
+
+  assert.bothAtTheSameTime(emitterReady, emitterInterval, acknowledgement);
 });
 
 test('promises: ready fires once then interval starts firing', assert => {
@@ -84,7 +108,7 @@ test('promises: ready fires once then interval starts firing', assert => {
   Promise.all([readyPromise, intervalPromise]).then( _values => {
     acknowledgement();
     emitterInterval.on('interval', acknowledgement);
-  })
+  });
 
   assert.readyThenIntervals(emitterReady, emitterInterval, acknowledgement);
 });
@@ -110,6 +134,29 @@ test('promises: interval starts firing then ready fires once', assert => {
   })
 
   assert.intervalsThenReady(emitterReady, emitterInterval, acknowledgement);
+});
+
+test('promises: both interval and ready start firing at the same time', assert => {
+  const emitterReady = new EventEmitter(),
+        emitterInterval = new EventEmitter(),
+        acknowledgement = td.function('.acknowledgement');
+
+  const readyPromise = new Promise( (resolve, reject) => {
+    emitterReady.once('ready', ()=>{
+      resolve()
+    });
+  });
+  const intervalPromise = new Promise( (resolve, reject) => {
+    emitterInterval.once('interval', ()=>{
+      resolve();
+    });
+  });
+  Promise.all([readyPromise, intervalPromise]).then( _values => {
+    acknowledgement();
+    emitterInterval.on('interval', acknowledgement);
+  })
+
+  assert.bothAtTheSameTime(emitterReady, emitterInterval, acknowledgement);
 });
 
 test('async await: ready fires once then interval starts firing', assert => {
@@ -139,7 +186,7 @@ test('async await: ready fires once then interval starts firing', assert => {
   })();
 
   assert.readyThenIntervals(emitterReady, emitterInterval, acknowledgement);
-})
+});
 
 test('async await: interval starts firing then ready fires once', assert => {
   const emitterReady = new EventEmitter(),
@@ -147,30 +194,47 @@ test('async await: interval starts firing then ready fires once', assert => {
         acknowledgement = td.function('.acknowledgement');
 
   const readyPromise = new Promise( resolve => {
-    emitterReady.once('ready', ()=>{
-      resolve(true)
-    });
+    emitterReady.once('ready', () => resolve(true) );
   });
 
   const intervalPromise = new Promise( resolve => {
-    emitterInterval.once('interval', ()=>{
-      resolve(true)
-    });
+    emitterInterval.once('interval', () => resolve(true) );
   });
 
   (async ()=>{
-    const ready = await readyPromise;
-    const interval = await intervalPromise;
+    const [ready, interval] = [await readyPromise, await intervalPromise];
     if (ready && interval) {
       acknowledgement();
       emitterInterval.on('interval', acknowledgement);
     }
   })();
 
-
-
   assert.intervalsThenReady(emitterReady, emitterInterval, acknowledgement);
-})
+});
+
+test('async await: both interval and ready start firing at the same time', assert => {
+  const emitterReady = new EventEmitter(),
+        emitterInterval = new EventEmitter(),
+        acknowledgement = td.function('.acknowledgement');
+
+  const readyPromise = new Promise( resolve => {
+    emitterReady.once('ready', () => resolve(true) );
+  });
+
+  const intervalPromise = new Promise( resolve => {
+    emitterInterval.once('interval', () => resolve(true) );
+  });
+
+  (async ()=>{
+    const [ready, interval] = [await readyPromise, await intervalPromise];
+    if (ready && interval) {
+      acknowledgement();
+      emitterInterval.on('interval', acknowledgement);
+    }
+  })();
+
+  assert.bothAtTheSameTime(emitterReady, emitterInterval, acknowledgement);
+});
 
 
 test.Test.prototype.readyThenIntervals = function(emitterReady, emitterInterval, ver){
@@ -201,4 +265,19 @@ test.Test.prototype.intervalsThenReady = function(emitterReady, emitterInterval,
     this.pass('called four times');
     this.end();
   }, 421);
-}
+};
+
+test.Test.prototype.bothAtTheSameTime = function(emitterReady, emitterInterval, ver){
+  setTimeout(()=>{
+    emitterReady.emit('ready');
+  }, 100);
+  setInterval(()=>{
+    emitterInterval.emit('interval');
+  }, 100);
+
+  setTimeout(()=>{
+    td.verify(ver(), {times: 4});
+    this.pass('called four times');
+    this.end();
+  }, 421);
+};
